19c8713ba4edc4f888e2b2a0a82a2d1e2d55104d
kkm <kubasurdej@gmail.com>
2020-08-08 00:42:17 +0200

    Fix more type errors

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,17 @@
 import { Client } from "discord.js";
 import { commands } from "./commandHandler";
+import config from "./config";
 
 const client = new Client();
-const prefix = process.env.prefix || "$";
 
 client.on("ready", () => {
   console.log(`Bot logged in as ${client.user?.tag}`);
+  console.log(`Bot prefix is set to '${config.prefix}'`);
 });
 
 client.on("message", async (msg) => {
-  if (msg.content && msg.content.startsWith(prefix)) {
-    const text = msg.content.slice(prefix.length);
+  if (msg.content && msg.content.startsWith(config.prefix)) {
+    const text = msg.content.slice(config.prefix.length);
     const command = text.split(" ")[0].toLowerCase();
     const args = text.slice(command.length).trim();
 
--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 const config = {
   token: process.env.DISCORD_TOKEN,
+  prefix: process.env.prefix || "$",
 };
 
 export default config;
