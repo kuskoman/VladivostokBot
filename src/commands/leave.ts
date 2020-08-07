273d28e233f36ad5db0fd22b13c4913719925a0a
kkm <kubasurdej@gmail.com>
2020-08-08 00:08:28 +0200

    Move leaving channel to utils

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -1,21 +1,10 @@
 import { Command } from "../commandHandler";
-import logger from "../logger";
+import { leaveChannel } from "../utils/connectionUtils";
 
 const leave: Command = {
   aliases: ["leave", "l"],
   executor({ msg }) {
-    if (!msg.guild?.id) {
-      return msg.channel.send("Can't find server id.");
-    }
-
-    const voiceChannel = msg.member?.voice?.channel;
-    if (!voiceChannel) {
-      return msg.channel.send(
-        `Can't find voice channel to leave. Join channel and try again.`
-      );
-    }
-    voiceChannel.leave();
-    logger.info(`Left channel ${msg.channel.id}`);
+    leaveChannel(msg);
   },
 };
 
--- src/utils/connectionUtils.ts
+++ src/utils/connectionUtils.ts
@@ -1,18 +1,18 @@
-import { Message } from "discord.js";
+import { Message, VoiceChannel } from "discord.js";
 import logger from "../logger";
 
-export const joinChannel = async (msg: Message) => {
+export const joinChannel = async (msg: Message): Promise<VoiceChannel> => {
   const channel = msg.member?.voice?.channel;
   if (!channel) {
     msg.channel.send("You must be in a voice channel first");
-    return false;
+    return null;
   }
 
   if (!channel.guild?.me?.hasPermission("CONNECT")) {
     msg.channel.send(
       "Bot does not have permission to join channels on this server."
     );
-    return false;
+    return null;
   }
 
   try {
@@ -26,6 +26,21 @@ export const joinChannel = async (msg: Message) => {
     );
     logger.info(`Could not join channel. Error: ${e}`);
 
-    return false;
+    return null;
   }
+};
+
+export const leaveChannel = async (msg: Message) => {
+  if (!msg.guild?.id) {
+    return msg.channel.send("Can't find server id.");
+  }
+
+  const voiceChannel = msg.member?.voice?.channel;
+  if (!voiceChannel) {
+    return msg.channel.send(
+      `Can't find voice channel to leave. Join channel and try again.`
+    );
+  }
+  voiceChannel.leave();
+  logger.info(`Left channel ${msg.channel.id}`);
 };
